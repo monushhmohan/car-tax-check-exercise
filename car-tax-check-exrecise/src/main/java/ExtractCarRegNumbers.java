cbde59fcd9b15de50d0557d4db3bea065d10ead7
Your Name <you@example.com>
2021-08-31 17:19:42 +0100

    added string variables to read and write external files and new method

---
+++ car-tax-check-exrecise/src/main/java/ExtractCarRegNumbers.java
@@ -0,0 +1,14 @@
+
+public class ExtractCarRegNumbers {
+
+	public static void main(String[] args) {
+		String sourcefile = "src/test/resources/car_input.txt";
+		String targetfile = "src/test/resources/car_registration_numbers.csv";
+		extractRegNoFromSrctoTrg(sourcefile,targetfile);
+	}
+
+	private static void extractRegNoFromSrctoTrg(String sourcefile, String targetfile) {
+
+	}
+
+}
