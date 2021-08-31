a5edb4026051dd2719a1fb656f9e78f2839f1209
Your Name <you@example.com>
2021-08-31 21:59:45 +0100

    added new methods with testng annotations in the main solution class

--- car-tax-check-exrecise/src/test/java/tests/SolutionClass.java
+++ car-tax-check-exrecise/src/test/java/tests/SolutionClass.java
@@ -1,7 +1,27 @@
 package tests;
 
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
 public class SolutionClass {
 	
-	public void 
+	@BeforeClass
+	public void testSetUp() {
+		
+	}
+	
+	@Test
+	public void carTaxCheckTest() {
+		
+	}
+	
+	@AfterTest
+	public void tearDown() {
+		
+	}
+	
+	
+	
 
 }
