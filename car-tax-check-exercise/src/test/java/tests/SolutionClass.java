bf2c52c9f415a0b63dba9875139993711c094a19
Your Name <you@example.com>
2021-08-31 22:54:01 +0100

    added csv paths and their corresponsing read write code

--- car-tax-check-exercise/src/test/java/tests/SolutionClass.java
+++ car-tax-check-exercise/src/test/java/tests/SolutionClass.java
@@ -2,6 +2,8 @@ package tests;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
@@ -11,11 +13,14 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
 
 
 public class SolutionClass {
 	private WebDriver driver = null;
 	String projectPath = System.getProperty("user.dir");
+	String[] csvCell;
+	String carReg = "null";
 	
 	@BeforeClass
 	public void testSetUp() {
@@ -26,15 +31,35 @@ public class SolutionClass {
 	}
 	
 	@Test
-	public void carTaxCheckTest() throws FileNotFoundException {
+	public void carTaxCheckTest() throws IOException, CsvValidationException {
 		
 		//Set path to csv file to fetch car registration numbers
-		String csv_path = projectPath+"\\src\\test\\resources\\car_registration_numbers.csv";
+		String csv_path = projectPath+"/src/test/resources/car_registration_numbers.csv";
 		//Create an object of CSVReader to link to above csv
 		CSVReader csvReader = new CSVReader(new FileReader(csv_path));
 		
+		//Create csv file to write vehicle details e.g. reg no., make, model etc.
+		String vd_csv = "src/test/resources/vehicledetails.csv";
+		//Create FileWriter object and link to the above vehicle details csv
+		FileWriter myWriter = new FileWriter(vd_csv);
+		String heading = "REGISTRATION,MAKE,MODEL,COLOR,YEAR\n";
+		myWriter.write(heading);
+		
+		//Create CSV to write incorrect car registrations 
+		String fail_csv = "src/test/resources/incorrectCarRegs.csv";
+		FileWriter failWriter = new FileWriter(fail_csv);
+		failWriter.write("REGISTRATION\n");
+		
+		//Loop to execute all the car registrations from csv
+		while((csvCell = csvReader.readNext()) != null){
+			for (int i=0;i<csvCell.length;i++) {
+				carReg = csvCell[i];
+			}
+		}
+		
 	}
-	
+
+
 	@AfterTest
 	public void tearDown() {
 		
